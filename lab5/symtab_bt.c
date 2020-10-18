cd473cd064cadaa38fa6af7a88cb85b3c704f5a0
Eveneko <huyubin0203@gmail.com>
2020-10-18 23:36:10 +0800

    lab5(bug): symtab_bt

--- lab5/symtab_bt.c
+++ lab5/symtab_bt.c
@@ -43,7 +43,7 @@ int symtab_insert(symtab *self, char *key, VAL_T value){
             }
             p = p->left;
         }else{
-            free(node);
+            // free(node);
             return 0;
         }
     }
@@ -57,94 +57,48 @@ VAL_T symtab_lookup(symtab *self, char *key){
         if(p->right == NULL){
             return -1;
         }
-        symtab_lookup(p->right, key);
+        return symtab_lookup(p->right, key);
     }else{
         if(p->left == NULL){
             return -1;
         }
-        symtab_lookup(p->left, key);
+        return symtab_lookup(p->left, key);
     }
+    return -1;
+}
+
+int flag = 1;
+
+symtab* _symtab_remove(symtab *self, char *key){
+    if(self == NULL){
+        flag = 0;
+        return NULL;
+    }
+    if(strcmp(key, self->entry.key) == 0){
+        if(self->left == NULL){
+            return self->right;
+        }else if(self->right == NULL){
+            return self->left;
+        }else{
+            symtab* cur = self->right;
+            while(cur->left != NULL){
+                cur = cur-> left;
+            }
+            cur->left = self->left;
+            self = self->right;
+            return self;
+        }
+    }
+    if(strcmp(key, self->entry.key) > 0){
+        self->right = _symtab_remove(self->right, key);
+    }else if(strcmp(key, self->entry.key) < 0){
+        self->left = _symtab_remove(self->left, key);
+    }
+    return self;
 }
 
 int symtab_remove(symtab *self, char *key){
-    symtab *p = self;
-    symtab *pre = NULL;
-
-    // 查找目标节点的位置
-    while(1){
-        if(strcmp(key, p->entry.key) > 0 && p->right != NULL){
-            pre = p;
-            p = p->right;
-        }else if(strcmp(key, p->entry.key) < 0 && p->left != NULL){
-            pre = p;
-            p = p->left;
-        }else if(strcmp(key, p->entry.key) !=0 && p->right == NULL && p->left == NULL){
-            return 0;   
-        }else{
-            break;
-        }
-    }
-    // 目标节点没有子节点
-    if(p->right == NULL && p->left == NULL){
-        if(p == pre->right){
-            pre->right = NULL;
-        }else if(p == pre->left){
-            pre->left = NULL;
-        }
-        free(p);
-    }
-    // 目标节点有左右子节点，按照中序删除
-    if(p->right != NULL && p->left != NULL){
-        symtab *del_p = p;
-        p = p->right;
-        if(p->right == NULL){
-            del_p->entry = p->entry;
-            if(p->right == NULL){
-                del_p->right = NULL;
-            }else{
-                del_p->right = p->right;
-            }
-            free(p);
-        }
-        while(p->left != NULL){
-            pre = p;
-            p = p->left;
-        }
-        if(p->right == NULL){
-            pre->left = NULL;
-        }else{
-            pre->left = p->right;
-        }
-        del_p->entry = p->entry;
-        free(p);
-    }
-    // 目标节点只有右节点
-    if(p->right != NULL){
-        if(pre != NULL){
-            if(p == pre->left){
-                pre->left = p->right;
-            }else if(p == pre->right){
-                pre->right = p->right;
-            }
-            free(p);
-        }else{
-            self = p->right;
-            free(p);
-        }
-    }
-    // 目标节点只有左子节点
-    if(p->left != NULL){
-        if(pre != NULL){
-            if(p == pre->left){
-                pre->left = p->left;
-            }else if(p == pre->right){
-                pre->right = p->left;
-            }
-            free(p);
-        }else{
-            self = p->left;
-            free(p);
-        }
-    }
-    return 1;
+    flag = 1;
+    _symtab_remove(self, key);
+    return flag;
 }
